26297c4075feaa0d188e3c29275977573780eb0d
Yael Peet <yaelpeet@gmail.com>
2023-09-11 09:26:20 -0700

    added an algo to weird string

---
+++ CodeWars/weirdString.js
@@ -0,0 +1,11 @@
+function toWeirdCase(string) {
+    string = string.split(' ').map(word => {
+        let weirdString = '';
+        for (let i = 0; i < word.length; i++) {
+            weirdString += (i % 2 === 0) ? word[i].toUpperCase() : word[i].toLowerCase();
+        }
+        return weirdString;
+    }).join(' ');
+
+    return string;
+}
